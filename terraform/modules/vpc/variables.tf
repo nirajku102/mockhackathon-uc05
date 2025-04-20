afb65d86c707d28943e927222d4976d1d9a6611a
nirajku102 <105897623+nirajku102@users.noreply.github.com>
2025-04-20 21:12:46 +0000

    modifying instance

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -8,11 +8,6 @@ variable "public_subnets_cidr" {
   type        = list(string)
 }
 
-variable "private_subnets_cidr" {
-  description = "The CIDR blocks for the private subnets"
-  type        = list(string)
-}
-
 variable "availability_zones" {
   description = "The availability zones for the subnets"
   type        = list(string)
