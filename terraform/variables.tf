33ca02c2611613bf89916f6f9ec1ddad3d4c3c04
nirajku102 <105897623+nirajku102@users.noreply.github.com>
2025-04-20 20:56:45 +0000

    modifying instance

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,8 @@ variable "key_pair" {
   description = "Key pair name for SSH access"
   type        = string
 }
+
+variable "subnet_id" {
+  description = "Subnet ID for the EC2 instance"
+  type        = string
+}
