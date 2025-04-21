0e7805552c51fdf5f8743cc1abf2d48dd044d118
nirajku102 <105897623+nirajku102@users.noreply.github.com>
2025-04-21 06:13:09 +0000

    [create-pull-request] automated change

--- terraform/dev.tfvars
+++ terraform/dev.tfvars
@@ -1,3 +1,3 @@
-ami               = "ami-08f78cb3cc8a4578e"
-instance_type     = "t3.micro"
-key_pair          = "hackathon-key"
+ami           = "ami-08f78cb3cc8a4578e"
+instance_type = "t3.micro"
+key_pair      = "hackathon-key"
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source               = "./modules/vpc"
+  source              = "./modules/vpc"
   vpc_cidr            = var.vpc_cidr
   public_subnets_cidr = var.public_subnets_cidr
   availability_zones  = var.availability_zones
@@ -12,13 +12,13 @@ module "security_group" {
 }
 
 module "ec2" {
-  source          = "./modules/ec2"
-  ami             = var.ami
-  instance_type   = var.instance_type
-  key_pair        = var.key_pair
-  security_group  = module.security_group.security_group_id
-  subnet_id       = module.vpc.public_subnets[0]
-  user_data       = base64encode(templatefile("${path.module}/user_data.sh", {}))
+  source         = "./modules/ec2"
+  ami            = var.ami
+  instance_type  = var.instance_type
+  key_pair       = var.key_pair
+  security_group = module.security_group.security_group_id
+  subnet_id      = module.vpc.public_subnets[0]
+  user_data      = base64encode(templatefile("${path.module}/user_data.sh", {}))
 }
 
 
